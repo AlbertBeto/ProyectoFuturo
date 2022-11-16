af71abffa6b3f3a68980438d648174c0cb374e86
Beto <beto@beto.cat>
2022-11-16 23:13:39 +0100

    Realizados los cambios del ejercicio con la rama creada y unidaa main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-
+//Me obligan a poner Creado por Albert Beto
     public static void main(String[] args) {
 //Aquí crearia un Scanner y variables para guardar las entradas de las variables a usar en el metodo.
         System.out.println(ProyectoFuturo.HorasEstudio("Albert Perez",6,23));
